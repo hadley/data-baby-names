f4ffea097aaf8aac5d56aa34440827c9a1874ff9
hadley <h.wickham@gmail.com>
2009-05-15 09:48:29 -0500

    Save out names as csv file

--- 3-clean.r
+++ 3-clean.r
@@ -3,6 +3,9 @@ library(plyr)
 files <- dir("raw", full = T)
 names(files) <- gsub("\\.csv", "", dir("raw"))
 
+# Load all csv files into a single data frame and give informative column
+# names
+
 bnames <- ldply(files, read.csv, header = F, skip = 1, nrows = 1000,
   stringsAsFactors = FALSE)
 names(bnames) <- c("year", "rank", "boy_name", "boy_percent", "girl_name", "girl_percent")
@@ -17,8 +20,9 @@ girls$sex <- "girl"
 
 all <- rbind(boys, girls)
 
-# Turn percent string into a number
+# Turn year and percent into a real numbers
 all$percent <- as.numeric(gsub("%", "", all$percent)) / 100
 all$year <- as.numeric(as.character(all$year))
 
-write
+# Save as csv
+write.table(all, "baby-names.csv", sep=",", row = F)
