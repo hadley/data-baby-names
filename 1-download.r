4b633dd682838e624608aae54cb6ec2894166960
Eric Socolofsky <eric@transmote.com>
2016-05-14 11:09:58 -0700

    SSA as SSL Update end year

--- 1-download.r
+++ 1-download.r
@@ -3,11 +3,11 @@ library(plyr)
 library(XML)
 
 save_year <- function(year) {
-  url <- "http://www.ssa.gov/cgi-bin/popularnames.cgi"
+  url <- "https://www.ssa.gov/cgi-bin/popularnames.cgi"
   data <- postForm(url, style = "post", 
     "number" = "p", "top" = "1000", "year" = year) 
   writeLines(data, paste("original/", year, ".html", sep=""))
 }
 
-years <- 1880:2008
+years <- 1880:2015
 l_ply(years, save_year)
