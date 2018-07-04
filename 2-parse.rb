3f8f13ccddec3385ecefc47f715cd4d696793bd4
graemebaird <gbaird@ucsc.edu>
2018-07-04 11:00:09 -0700

    Change in SSA table struct

--- 2-parse.rb
+++ 2-parse.rb
@@ -3,7 +3,7 @@ require "FasterCSV"
 
 def parse_year(year) 
   doc = Nokogiri::HTML(open("original/#{year}.html"))
-  rows = (doc/"body/table[2]/tr/td[2]/table/tr")
+  rows = (doc/"body/table[3]/tr/td[2]/table/tr")
   
   FasterCSV.open("raw/#{year}.csv", "w") do |csv|
     rows.each {|row| csv << parse_row(row)}
